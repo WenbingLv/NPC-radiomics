5ef264552eea385bef0c0955e8c7db9b12b3d697
WenbingLv <33389477+WenbingLv@users.noreply.github.com>
2017-11-06 14:57:10 +0800

    Update demo_FeaExtr.m

--- demo_FeaExtr.m
+++ demo_FeaExtr.m
@@ -1,6 +1,6 @@
 % This is a matlab demo showing how to construct radiomic matrix 
 %and extract radiomic features using different strategies/parameter 
-%settings for PET/CT scaned NPC images. 
+%settings for PET/CT scanned NPC images. 
 %
 % W.Lv, Q.Yuan et al. Impact of parameter settings for radiomic features on their robustness 
 % and diagnostic task performance: application to nasopharyngeal 18F-FDG PET/CT.
